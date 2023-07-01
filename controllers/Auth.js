87c787ce2063566e83e22783fc48d8be51048a50
tukiso <tkmateka@.com>
2023-07-01 11:23:37 +0200

    Application complete

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -5,29 +5,27 @@ const bcrypt = require('bcrypt')
 const Employee = require('../models/Employee');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_CONNECTION).then(console.log('connected to DB')).catch((err) => console.log(err));
+mongoose.connect(process.env.MONGODB_CONNECTION).then(console.log('connected to DB')).catch((err) => console.log(111111111, err));
 
 module.exports = {
     register: async (req, res) => {
         try {
-              // The second parameter is the default salt
-              const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  
-              let emp = { ...req.body };
-              emp.password = hashedPassword;
-  
-              const newEmployee = new Employee({ ...emp });
-              const result = await newEmployee.save();
-              
-              res.status(201).send({ message: "User registered successfully." });
-          } catch (e) {
-              res.status(500).send(e);
-          }
+            // The second parameter is the default salt
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+            let emp = { ...req.body };
+            emp.password = hashedPassword;
+
+            const newEmployee = new Employee({ ...emp });
+            const result = await newEmployee.save();
+
+            res.status(201).send({ message: "You have registered successfully." });
+        } catch (e) {
+            res.status(500).send(e);
+        }
     },
     login: async (req, res) => {
         const user = await Employee.find({ email: req.body.email });
-
-        console.log("user", user);
 
         if (!user[0]) return res.status(400).send('User not found');
 
@@ -44,5 +42,28 @@ module.exports = {
         } catch (e) {
             res.status(500).send(e);
         }
+    },
+    update_user: async (req, res) => {
+        try {
+            const filter = { email: req.body.email };
+            const update = req.body;
+
+            const employee = await Employee.findOneAndUpdate(filter, update);
+            res.json(employee);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    },
+    get_users: async (req, res) => {
+        const users = await Employee.find()
+        res.json(users);
+    },
+    get_user_by_email: async (req, res) => {
+        const user = await Employee.find({ email: req.params.email })
+        res.json(user);
+    },
+    delete_user: async (req, res) => {
+        const user = await Employee.findOneAndRemove({ email: req.params.email })
+        res.json(user);
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/Auth');
+const uploadMiddleware = require('../middlewares/Upload');
+const uploadControllers = require('../controllers/Upload');
 
 // Auth Routes
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/update-user', authController.update_user);
+router.get('/users', authController.get_users);
+router.delete('/delete/:email', authController.delete_user);
+
+// File Upload Routes
+router.post('/upload', uploadMiddleware.upload.single('file'), uploadControllers.upload_file);
+router.get('/files', uploadControllers.get_files);
+router.get('/file/:filename', uploadControllers.get_file_by_filename);
+router.get('/image/:filename', uploadControllers.get_image_by_filename);
+router.get('/get_any_file_by_filename/:filename', uploadControllers.get_any_file_by_filename);
+router.delete('/file/:id', uploadControllers.delete_file_by_id);
 
 module.exports = router;
